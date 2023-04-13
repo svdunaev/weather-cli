4d736517313b5c4c38e41fb7ff97f29ebb2a80b1
svdunaev <54572104+svdunaev@users.noreply.github.com>
2023-04-14 01:25:30 +0500

    feat: add logic for reading and writing data

--- weather.js
+++ weather.js
@@ -1,24 +1,36 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js'
-import { printHelp } from './services/log.service.js';
-import { saveKeyValue } from './services/storage.service.js';
+import { printHelp, printSuccess, printError } from './services/log.service.js'
+import { saveKeyValue } from './services/storage.service.js'
+
+const saveToken = async (token) => {
+	if(!token.length) {
+		printError('Token required')
+		return
+	}
+  try {
+    await saveKeyValue('token', token)
+    printSuccess('Token saved')
+  } catch (error) {
+    printError(error.message)
+  }
+}
 
 const initCLI = () => {
-	const args = getArgs(process.argv)
+  const args = getArgs(process.argv)
 
-	if(args.h) {
-		printHelp();
-	}
-	if(args.s) {
-		// save city
-		
-	} 
-	if(args.t) {
-		// save token
-		saveKeyValue('token', args.t);
-	}
+  if (args.h) {
+    printHelp()
+  }
+  if (args.s) {
+    // save city
+  }
+  if (args.t) {
+    // save token
+    return saveToken(args.t)
+  }
 
-	// return weather report
-};
+  // return weather report
+}
 
-initCLI();
+initCLI()
